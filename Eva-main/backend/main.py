27a8b16df233ec9e4a216eaff7c913e51a9d8275
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-11-02 11:30:00 +0530

    feat:pushed changes

--- Eva-main/backend/main.py
+++ Eva-main/backend/main.py
@@ -10,7 +10,49 @@ from pydantic import BaseModel
 import chromadb
 from . import models, schemas
 from .database import engine, SessionLocal
-from ...open import start_listening, audio_to_text
+from dotenv import load_dotenv
+import os
+from elevenlabs.client import ElevenLabs
+import io
+load_dotenv()
+
+elevenlabs = ElevenLabs(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
+)
+
+def audio_to_text1(audio_path):
+    try:
+        with open(audio_path, "rb") as f:
+            transcription = elevenlabs.speech_to_text.convert(
+                file=f,
+                model_id="scribe_v1",
+                tag_audio_events=False,
+                diarize=False
+            )
+        return transcription.text
+    except Exception as e:
+        print("Error transcribing audio:", e)
+        return None
+
+
+def audio_to_text(audio_data):
+    try:
+        # Convert recorded audio to BytesIO
+        audio_file = io.BytesIO(audio_data.get_wav_data())
+
+        # Call ElevenLabs speech-to-text
+        transcription = elevenlabs.speech_to_text.convert(
+            file=audio_file,
+            model_id="scribe_v1",      # ElevenLabs transcription model
+            tag_audio_events=False,    # optional: True if you want events
+            # language_code="eng",
+            diarize=False              # optional: True if multiple speakers
+        )
+
+        return transcription.text
+    except Exception as e:
+        print("Error transcribing audio:", e)
+        return None
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -27,7 +69,7 @@ app.add_middleware(
 current_transcript = ""
 wake_detected = False
 transcript_lock = threading.Lock()
-
+'''
 def background_listener():
     global current_transcript, wake_detected
     print("🎧 Wake listener active.")
@@ -47,10 +89,10 @@ def background_listener():
                         print("👂 Wake word detected: EVA")
         except Exception as e:
             print(f"🎙️ Listener error: {e}")
-
+'''
 @app.on_event("startup")
 def start_voice_assistant():
-    listener_thread = threading.Thread(target=background_listener, daemon=True)
+    listener_thread = threading.Thread(target=start_listening, daemon=True)
     listener_thread.start()
 
 def get_db():
